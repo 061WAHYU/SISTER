1d122400e72aa0b4c3ddd0a5611cfc8eadde1019
061WAHYU <114321607+061WAHYU@users.noreply.github.com>
2023-07-06 10:27:55 +0800

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Latihan</title>
+	<script src="https://cdn.tailwindcss.com"></script>
+		<!-- component -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<section style="font-family: Montserrat" class=" bg-[#071e34] flex font-medium items-center justify-center h-screen">
+
+    <section class="w-64 mx-auto bg-[#20354b] rounded-2xl px-10 py-6 shadow-lg">
+    	<div class="mt-6 w-fit mx-auto">
+            <img src="https://cdn.dribbble.com/users/1577045/screenshots/4914645/media/5146d1dbf9146c4d12a7249e72065a58.png" class="rounded-full w-28 " alt="profile picture" srcset="">
+        </div>
+        <div class="mt-8 ">
+            <h2 class="text-white font-bold text-2xl tracking-wide">Komang Wahyu Kusuma Wardana</h2>
+        </div>
+        <p class="text-emerald-400 font-semibold mt-2.5" >
+            Kelas B
+        </p>
+
+        <div class="h-1 w-full bg-black mt-8 rounded-full">
+            <div class="h-1 rounded-full w-2/5 bg-yellow-500 "></div>
+        </div>
+        <div class="mt-3 text-white text-sm">
+            <span class="text-gray-400 font-semibold">Storage:</span>
+            <span>40%</span>
+        </div>
+
+    </section>
+
+
+</section>
+</body>
+</html>
